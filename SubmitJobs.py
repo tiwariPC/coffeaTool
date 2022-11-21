bd51daede53e2a38da9fd7f1120e234f94d23482
Deepak Kumar <sh2deepakkumar@gmail.com>
2022-11-21 16:59:35 +0100

    updated files for condor

--- SubmitJobs.py
+++ SubmitJobs.py
@@ -2,15 +2,13 @@ from glob import glob
 import os,sys, optparse,argparse
 
 test=False
-path = sys.argv[2]
-
+path = sys.argv[4]
+tag=sys.argv[3]
+year  = sys.argv[2]
 files =glob(path+'/*root')
-fout = open('submit_condor-tmp2.sh','w')
-#fin  = open('submit_condor-tmp.sh','r')
 dataframefile = sys.argv[1]
-#dataframefile='DataFrameToHisto_V1_R_hemRPBased.py'
-#outputDir = sys.argv[3]
 #if not os.path.isdir(outputDir):os.system('mkdir '+str(outputDir))
+print ("total qurgumet",len(sys.argv))
 
 top='''#!/bin/sh
 ulimit -s unlimited
@@ -20,22 +18,23 @@ python3 -m venv my_env
 source my_env/bin/activate
 
 '''
+count=sys.argv[5]
+fout = open('submit_condor-tmp'+count+'.sh','w')
 fout.write(top+'\n')
 fout.write('\n')
 
 
 for ii,ifile in enumerate(files):
 	fout.write('if [ $1 -eq '+str(ii)+' ]; then'+'\n')
-	fout.write('	python  '+dataframefile+'  -i   '+ifile+'\n')#'     -D   '+outputDir+'\n')
+	fout.write('	python  '+dataframefile+' -m   -y '+year+'  -tag '+tag+'  -i   '+ifile+'\n')#'     -D   '+outputDir+'\n')
 	fout.write('fi'+'\n')
 
 fout.close()
 
 
 
-
-top2='''executable = submit_condor-tmp2.sh
-arguments = $(ProcId)
+top_='executable = submit_condor-tmp'+count+'.sh'
+top2='''arguments = $(ProcId)
 output                = output/condor.$(ClusterId).$(ProcId).out
 error                 = error/condor.$(ClusterId).$(ProcId).err
 log                   = log/condor.$(ClusterId).log
@@ -49,10 +48,15 @@ periodic_release =  (NumJobStarts < 3) && ((CurrentTime - EnteredCurrentStatus) 
 +JobFlavour="tomorrow"
 '''
 
-fout2=open('submit_condor-test.sub','w')
+filename = 'submit_condor-test'+count+'.sub'
 
+fout2=open(filename,'w')
+fout2.write(top_+'\n')
 fout2.write(top2+'\n')
-fout2.write('queue  '+str(len(files)))
+fout2.write('queue  '+str(len(files))+'\n')
+fout2.close()
 
-
-if not test:os.system('condor_submit submit_condor-test.sub >& logjob.txt &')
+#if len(sys.argv)==6:
+#    os.system('cat submit_condor-tmp2.sh > info/submit_condor-tmp2'+sys.argv[5]+'.sh')
+if not test:os.system('condor_submit '+filename)
+# >& logjob.txt &')
